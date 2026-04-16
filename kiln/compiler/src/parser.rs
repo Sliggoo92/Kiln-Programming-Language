d63c53a190b70ac365374fce606bb261425b507f
Benjamin Miller <bemiller0712@gmail.com>
2026-04-16 18:32:07 -0400

    Update parser.rs

--- kiln/compiler/src/parser.rs
+++ kiln/compiler/src/parser.rs
@@ -1,1 +1,24 @@
+use kiln_core::token::Token;
+use kiln_core::ast::{Expr, Stmt};
 
+pub struct Parser {
+    tokens: Vec<Token>,
+    position: usize,
+}
+
+impl Parser {
+    pub fn new(tokens: Vec<Token>) -> Self {
+        Self {
+            tokens,
+            position: 0,
+        }
+    }
+
+    fn current(&self) -> Option<&Token> {
+        self.tokens.get(self.position)
+    }
+
+    fn advance(&mut self) {
+        self.position += 1;
+    }
+}
