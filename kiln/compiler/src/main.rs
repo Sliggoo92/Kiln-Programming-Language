3f6c7c55f9ed057d42ad3481532df359a69d9a33
Benjamin Miller <bemiller0712@gmail.com>
2026-04-16 18:35:46 -0400

    add parser to main.rs

--- kiln/compiler/src/main.rs
+++ kiln/compiler/src/main.rs
@@ -2,10 +2,18 @@ mod lexer;
 mod parser;
 
 use lexer::Lexer;
+use parser::Parserl
 
 fn main() {
-    let mut lexer = Lexer::new("+-");
+    let source = "42";
+    
+    let mut lexer = Lexer::new(source);
+    let tokens = lexer.lex();
 
+    let mut parser = Parser::new(tokens);
+    
+    println!("Parser initialized!");
+    
     loop {
         let token = lexer.next_token();
         println!("{:?}", token);
