6ca33e50da23fca03d3b4c9946f9d68d1a0571e4
Benjamin Miller <bemiller0712@gmail.com>
2026-04-17 14:41:58 -0400

    connect parser to main.rs

--- kiln/compiler/src/main.rs
+++ kiln/compiler/src/main.rs
@@ -1,25 +1,38 @@
 mod lexer;
 mod parser;
+mod ast;
+mod compiler;
 
+use ast::TopLevel;
+use compiler::Compiler;
 use lexer::Lexer;
-use parser::Parserl
+use parser::Parser;
 
 fn main() {
-    let source = "42";
-    
-    let mut lexer = Lexer::new(source);
-    let tokens = lexer.lex();
+    let context = inkwell::context::Context::create();
+    let mut compiler = Compiler::new(&context, "my_program");
 
+    // source code of your program
+    let source = std::fs::read_to_string("program.kiln").expect("could not read file");
+    
+    let mut lexer = Lexer::new(&source);
+    let tokens = lexer.tokenize();
     let mut parser = Parser::new(tokens);
-    
-    println!("Parser initialized!");
-    
-    loop {
-        let token = lexer.next_token();
-        println!("{:?}", token);
+    let ast = parser.parse_program().expect("parse error");
 
-        if token == kiln_core::token::Token::EOF {
-            break;
+    for item in &ast {
+        match item {
+            TopLevel::Func(func_def) => {
+                compiler.codegen_func(func_def).expect("codegen error");
+            }
+            TopLevel::Use(_) => {
+                // module resolution handled separately
+            }
+            _ => {}
         }
     }
+
+    // emit LLVM IR to stdout for now
+    compiler.module.print_to_stderr();
+
 }
